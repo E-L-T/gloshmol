93272455b7893f3d29e47c6b07aa6dc14256d525
E-L-T <ericleroyterquem@yahoo.fr>
2017-10-10 23:20:33 +0200

    Rassemblé les deux fichiers CSS de la page realisation. Ajouté les modifs de la page JS et CSS (aside) dans realisation.

--- js/gloshmol_realisations.js
+++ js/gloshmol_realisations.js
@@ -1,23 +1,20 @@
+var mediaLimitSize = 900;
+
 function filtrerSommaire(filtre, realisationChoix) {
     //gestionnaire d'événement "click"
+    //si je ne suis pas sur la page index.html, allons y. Attendons que la page se charge, puis activons les filtres (la suite des instructions de cette fonction)
     var filtreElt = document.getElementById(filtre);
     filtreElt.addEventListener("click", function() {
         //commence par (ré)afficher l'ensemble des éléments
         var realisationsElts = document.getElementById("realisations");
         var integraleRealisationElts = realisationsElts.querySelectorAll(".realisations, .realisation, .exposition, .dessin, .installation, .sculpture, .livret");
         for (i = 0; i < integraleRealisationElts.length; i ++) {
-           if (window.innerWidth < 900) {
-               integraleRealisationElts[i].style.display = "flex";
-           } else if (window.innerWidth > 900) {
-                integraleRealisationElts[i].style.display = "block";
-           }   
+            integraleRealisationElts[i].style.display = "block";   
         }
         //supprime les éléments ne faisant pas partie de la catégorie sélectionnée
-        if (filtre !== "filtreTout") {
-            var excludedElts = realisationsElts.querySelectorAll("div:not(." + realisationChoix + ")"); 
-            for (i = 0; i < excludedElts.length; i++) {
-                excludedElts[i].style.display = "none";
-            }
+        var excludedElts = realisationsElts.querySelectorAll("div:not(." + realisationChoix + ")"); 
+        for (i = 0; i < excludedElts.length; i++) {
+            excludedElts[i].style.display = "none";
         }
         //affiche systématiquement le texte de présentation des réalisations
         var texteRealisationsElts = document.querySelectorAll(".texteRealisation");
@@ -34,149 +31,36 @@ filtrerSommaire("filtreSculpture", "sculpture");
 filtrerSommaire("filtreLivret", "livret");
 /*filtrerSommaire("filtreTout", "");
 */
-//Apparition de la liste de filtres au hover du menu. Remplacé par du CSS
-/*function dropDownMenu(idMenu, idContenu) {
-    var contenuElt = document.getElementById(idContenu);
-    var menuElt = document.getElementById(idMenu);
-    menuElt.addEventListener("mouseover", function() {
-        contenuElt.style.display = "block";
-    });
-    contenuElt.addEventListener("mouseover", function() {
-        contenuElt.style.display = "block";
-    });
-    contenuElt.addEventListener("mouseout", function() {
-        contenuElt.style.display = "none";
-    });
-    menuElt.addEventListener("mouseout", function() {
-        contenuElt.style.display = "none";
-    });
-}
-if (window.innerWidth > 900) { //pour éviter effet non voulu en version mobile
-    dropDownMenu("menuRealisations", "filtres");
-    dropDownMenu("menuAPropos", "aPropos");
-}
-*/
-
-
-//gestionnaire d'événement pour changer le logo en dessous et au-dessus de 900 pixels de largeur
-//aide trouvée ici : https://stackoverflow.com/questions/641857/javascript-window-resize-event
-
-
-var addEvent = function(object, type, callback) {
-    if (object == null || typeof(object) == 'undefined') return;
-    if (object.addEventListener) {
-        object.addEventListener(type, callback, false);
-    } else if (object.attachEvent) {
-        object.attachEvent("on" + type, callback);
-    } else {
-        object["on"+type] = callback;
-    }
-};
-
-addEvent(window, "resize", function() {
-    if (window.innerWidth < 900) {
-        if (document.getElementById("iconeCroix").style.display === "none") {
-            //document.getElementById("realisations").style.display = "block";
-            document.querySelector("aside").style.display = "none";
-
-        } else if (document.getElementById("iconeCroix").style.display === "block") {
-            //document.getElementById("realisations").style.display = "none";
-            document.querySelector("aside").style.display = "block";
-
-        }
-    //document.querySelector("aside").style.display = "none";
-    //document.getElementById("lienLogo").innerHTML = "gloshmol";
-    //document.getElementById("logo").style.textAlign = "center";
-    //afficherMenuV()
-    //enleverMenuV();
-    } else if (window.innerWidth > 900) {
-        if (document.getElementById("iconeCroix").style.display === "none") {
-            document.querySelector("aside").style.display = "block";
-            document.querySelector("nav").style.display = "none";
-            //document.getElementById("realisations").style.display = "flex";
-        } else if (document.getElementById("iconeCroix").style.display === "block") {
-            document.querySelector("aside").style.display = "block";
-            document.querySelector("nav").style.display = "block";
-            //document.getElementById("realisations").style.display = "flex";
-        }
-    //document.querySelector("aside").style.display = "block";
-    //document.getElementById("lienLogo").innerHTML = "-:-:-:-gloshmol-:-:-:-";
-    //document.getElementById("logo").style.textAlign = "left";
-    //Ajouter la fonction pour affichage normal du menu
-    //enleverMenuV();
-    //document.querySelector("nav").style.display = "block";
-    //document.getElementById("iconeMenu").style.display = "none";
-    //document.getElementById("iconeCroix").style.display = "none";
-    //afficherMenuH();
-    }
-});
-
-//J'ajoute ceci pour lancer l'instruction dès l'ouverture de la page
-/*if (window.innerWidth < 900) {
-    document.getElementById("lienLogo").innerHTML = "gloshmol";
-    document.getElementById("logo").style.textAlign = "center";
-}; 
-if (window.innerWidth > 900) {
-document.getElementById("lienLogo").innerHTML = "-:-:-:-gloshmol-:-:-:-";
-document.getElementById("logo").style.textAlign = "left";
-};*/
-
-function afficherMenuV() {
-    if (window.innerWidth > 900) {
-        document.getElementById("iconeMenu").style.display = "none";
-        document.querySelector("aside").style.display = "block";
-        document.querySelector("nav").style.display = "block";
-        document.getElementById("filtres").style.display = "block";//css
-        document.getElementById("aPropos").style.display = "block";
-        document.getElementById("iconeCroix").style.display = "block";
-        //document.querySelectorAll("realisation").style.display = "block";
-    } else if (window.innerWidth < 900) { //reprendre ici
-    //document.getElementById("logo").style.display = "none";
-    document.getElementById("iconeMenu").style.display = "none";
-    //document.getElementById("realisations").style.display = "none";
-    document.querySelector("aside").style.display = "block";
-    document.querySelector("nav").style.display = "block";
-    document.getElementById("filtres").style.display = "block";//css
-    document.getElementById("aPropos").style.display = "block";//css
-    document.getElementById("iconeCroix").style.display = "block";
-    //document.querySelectorAll("realisation").style.display = "flex";
-    }
-}
-
-function enleverMenuV() {
-    if (window.innerWidth > 900) {
-        document.getElementById("iconeMenu").style.display = "block";
-        document.querySelector("nav").style.display = "none";
-        document.querySelector("aside").style.display = "block";
-        document.getElementById("iconeCroix").style.display = "none";
-        //document.querySelectorAll("realisation").style.display = "block";
-    } else if (window.innerWidth < 900) {
-        document.getElementById("logo").style.display = "block";
-        document.getElementById("iconeMenu").style.display = "block";
-        document.querySelector("aside").style.display = "none";
-        //document.querySelector("nav").style.display = "none";
-        document.getElementById("iconeCroix").style.display = "none";
-        //document.getElementById("realisations").style.display = "block";
-        //document.querySelectorAll("realisation").style.display = "flex";
-    }    
-}
-
-
-
+//Déclaration de la fonction toggleMenu
+var iconeMenuElt = document.querySelector("#iconeMenu");
+/* console.log(iconeMenuElt);
+ */var asideElt = document.querySelector("aside");
+/* console.log(asideElt);
+ */var iconeCroixElt = document.querySelector("#iconeCroix");
+/* console.log(iconeCroixElt);
+ */
 //Apparition du menu vertical post clic menu burger
-document.getElementById("iconeMenu").addEventListener("click", function () {
-    afficherMenuV();
+iconeMenuElt.addEventListener("click", function(e){
+    e.preventDefault(); //j'ajoute cette ligne pour éviter de remonter automatiquement en haut de page lors d'un clic sur menu burger (ou icone croix, ci-dessous);
+    $('aside').animate({right:"0px"},600);
+    iconeMenuElt.classList.toggle("disappear");
+    iconeCroixElt.classList.toggle("appear");
 });
 
 //Disparition du menu vertical quand on clique sur la croix
-document.getElementById("iconeCroix").addEventListener("click", function() {
-    enleverMenuV();
+iconeCroixElt.addEventListener("click", function(e){
+    e.preventDefault();     
+    $('aside').animate({right:"-210px"},600);
+    iconeMenuElt.classList.toggle("disappear");
+    iconeCroixElt.classList.toggle("appear");
 });
 
 //Disparition du menu vertical quand on clique sur un lien dans le menu vertical
-
-document.querySelector("nav").addEventListener("click", function() {
-    if (window.innerWidth < 900) {
-        enleverMenuV();
+document.querySelector("nav").addEventListener("click", function(e) {
+/*     e.preventDefault(); Là, j'ai besoin de revenir au début de la liste
+ */    if (window.innerWidth < mediaLimitSize) {
+        $('aside').animate({right:"-210px"},600);
+        iconeMenuElt.classList.toggle("disappear");
+        iconeCroixElt.classList.toggle("appear");
     } 
 });
