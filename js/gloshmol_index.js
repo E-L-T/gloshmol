68d8634e74a8e6cb5807c9d843ad160cb5838d2f
E-L-T <ericleroyterquem@yahoo.fr>
2017-10-10 22:23:00 +0200

    Apparition du menu en vmobile via une animation Jquery. Empêché la remontée automatique en haut de page dans les gestionnaires d’evt « click » par e.preventDefault();

--- js/gloshmol_index.js
+++ js/gloshmol_index.js
@@ -31,13 +31,15 @@ filtrerSommaire("filtreLivret", "livret");
 
 //Déclaration de la fonction toggleMenu
 var iconeMenuElt = document.querySelector("#iconeMenu");
-console.log(iconeMenuElt);
-var asideElt = document.querySelector("aside");
-console.log(asideElt);
-var iconeCroixElt = document.querySelector("#iconeCroix");
-console.log(iconeCroixElt);
+/* console.log(iconeMenuElt);
+ */var asideElt = document.querySelector("aside");
+/* console.log(asideElt);
+ */var iconeCroixElt = document.querySelector("#iconeCroix");
+/* console.log(iconeCroixElt);
+ */
 
-function toggleMenu() {
+
+/* function toggleMenu() {
     iconeMenuElt.classList.toggle("disappear");
     asideElt.classList.toggle("appear");
     iconeCroixElt.classList.toggle("appear");
@@ -55,8 +57,130 @@ document.querySelector("nav").addEventListener("click", function() {
     if (window.innerWidth < mediaLimitSize) {
         toggleMenu();
     } 
+}); */
+/* 
+function addMenu(){
+    e.preventDefault(); 
+    $('aside').animate({right:"0px"},600);
+    iconeMenuElt.classList.toggle("disappear");
+    iconeCroixElt.classList.toggle("appear");
+}
+
+function removeMenu(){
+    e.preventDefault();     
+    $('aside').animate({right:"-210px"},600);
+    iconeMenuElt.classList.toggle("disappear");
+    iconeCroixElt.classList.toggle("appear");
+}
+//Apparition du menu vertical post clic menu burger
+iconeMenuElt.addEventListener("click", addMenu(e));
+
+//Disparition du menu vertical quand on clique sur la croix
+iconeCroixElt.addEventListener("click", removeMenu(e));
+
+//Disparition du menu vertical quand on clique sur un lien dans le menu vertical
+
+document.querySelector("nav").addEventListener("click", function() {
+    if (window.innerWidth < mediaLimitSize) {
+        removeMenu();
+    } 
+}); */
+
+//Apparition du menu vertical post clic menu burger
+iconeMenuElt.addEventListener("click", function(e){
+    e.preventDefault(); //j'ajoute cette ligne pour éviter de remonter automatiquement en haut de page lors d'un clic sur menu burger (ou icone croix, ci-dessous);
+    $('aside').animate({right:"0px"},600);
+    iconeMenuElt.classList.toggle("disappear");
+    iconeCroixElt.classList.toggle("appear");
 });
 
- 
-// scroll by pour descendre par palier
+//Disparition du menu vertical quand on clique sur la croix
+iconeCroixElt.addEventListener("click", function(e){
+    e.preventDefault();     
+    $('aside').animate({right:"-210px"},600);
+    iconeMenuElt.classList.toggle("disappear");
+    iconeCroixElt.classList.toggle("appear");
+});
 
+//Disparition du menu vertical quand on clique sur un lien dans le menu vertical
+document.querySelector("nav").addEventListener("click", function(e) {
+/*     e.preventDefault(); Là, j'ai besoin de revenir au début de la liste
+ */    if (window.innerWidth < mediaLimitSize) {
+        $('aside').animate({right:"-210px"},600);
+        iconeMenuElt.classList.toggle("disappear");
+        iconeCroixElt.classList.toggle("appear");
+    } 
+});
+
+//mousewheel effect
+// using on
+/* $('.realisation').on('mousewheel', function(event) {
+    console.log(event.deltaX, event.deltaY, event.deltaFactor);
+}); */
+
+/* var owl = $('#realisations');
+owl.on('mousewheel', function (e) {
+    var delta = event.deltaY;
+    var i = 0,
+        nextScrollTop 	= 0,
+        prevScrollTop 	= 0,
+        currScrollTop	= 0,
+        nextScrollable 	= true,
+        prevScrollable 	= true,
+        currIndex 		= 1,
+        nextIndex		= 2,
+        prevIndex		= numberOfSections;
+    /* $(this).scrollTop = "200"; */
+    /* if (e.deltaY>0) {
+        owl.trigger('next.owl');
+    } else {
+        owl.trigger('prev.owl');
+    }
+    e.preventDefault(); 
+}); */
+
+//Essai avec fullpage
+/* $(function(){
+    $('#realisations').fullpage();
+} */
+ 
+//essai de scroll par étape
+
+/* var Current_scroll_Y=0;
+var Target_scroll_Y=0;
+var scroll_Speed = 15;
+
+function animate_Scroll(Y)
+{
+  Target_scroll_Y = Y;
+  screen_Y = Math.floor(window.scrollY);
+
+  //Scroll Down
+  if(screen_Y<Y)
+  {
+    var myWindowScroll = setInterval(function(){ screen_Y = screen_Y+scroll_Speed;  if(screen_Y>=Target_scroll_Y){ clearInterval(myWindowScroll); return;}    window.scrollTo(0, screen_Y); }, 3);   
+  }
+  //Scroll Up
+  if(screen_Y>Y)
+  {
+    var myWindowScroll = setInterval(function(){ screen_Y = screen_Y-scroll_Speed;  if(screen_Y<=Target_scroll_Y){ clearInterval(myWindowScroll); return;}    window.scrollTo(0, screen_Y); }, 3);  
+  } 
+
+
+}//End animate_Scroll */
+
+/* 
+// scroll by pour descendre par palier
+var scrollStep = document.querySelector(".realisation").style.height;
+
+window.addEventListener("keydown", function() {
+    //animate_Scroll(10);
+    
+    //window.scrollBy(0, window.innerHeight*0.9); //ajusté avec une height des div realisation de 90vh
+    //this.scrollBy(0, 300); //tester scrollTop
+}); //au premier scroll, il se met à descendre tt en bas
+
+//gestionnaire d'evt scroll
+//attribuer taille de 100 vh à realisation en mobile
+//scrollBy 100vh
+//transition : 1s ease-in-out sur la div .realisation dans le css mobile */
