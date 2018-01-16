7841a826e61e293f5f8fa6f6e1824027187f7783
E-L-T <ericleroyterquem@yahoo.fr>
2018-01-16 17:45:54 +0100

    traitement du formulaire imagier uniquement dans le fichier imagier.php

--- imagier.php
+++ imagier.php
@@ -1,33 +1,55 @@
 <?php
-session_start();
 require_once('inc/init.inc.php');
 $page = 'Imagier';
 require_once('inc/haut.inc.php');
 $imagier_active = 'active';
 require_once('inc/header.index.inc.php');
 
+//Enregistrement des noms des fichiers images dans un tableau
+$dirImagier = '/var/www/html/gloshmol/imagier';
+
+$nomsImages = scandir($dirImagier);
+
+//sélection des fichiers images contenant la requete
+if(isset($_POST) && empty($_POST) == false) {
+    $requete = $_POST;
+    //var_dump($requete);
+    $requeteString = $requete['the_search'];
+    //echo $requeteString;
+    
+    foreach ($nomsImages as $nomImage) {
+        if (strpos($nomImage, $requeteString)) {
+            /* echo "Une correspondance pour " . $requeteString . " \n". " : " . $nomImage . " \n"; */
+            array_push($resultatImages, $nomImage);
+        }
+    }
+/*     var_dump($resultatImages);
+ */    
+}
+
 
 ?>
     
-            <div id="realisations">
                 
-                <div class="realisation">
-                    <form action="imagierTraitement.php" method="post">
-                        <input type="search" placeholder="Entrez un mot-clef" name="the_search">
-                        <input type="submit" value="Envoyer" />
+                <div id="realisations">
+                    <div class="formulaire realisation">
+                        <form action="imagier.php" method="post">
+                            <input class="inputImagier requeteImagier" type="search" placeholder="Mot-clé" name="the_search">
+                            <input class="inputImagier submitImagier" type="submit" value="Rechercher" />
 
-                    </form>
-                </div>
+                        </form>
+                        <?php 
+                            if(isset($_POST) && empty($_POST) == false) {
+                                echo '<span class="resultatImagier" >Voici les résultats de la recherche "' . $requeteString . '"</span>';
+                        }
+                        ?>
+                    </div>
 
-                <div class="realisation">
-                </div>
-
-                <div class="realisation"></div>
+                    <div class="realisation"></div>
+                    <div class="realisation"></div>
 
                 <?php 
-                    if($_SESSION){
-                        $resultatImages = $_SESSION['resultatImages'];
-
+                    if(isset($_POST)){
                         foreach ($resultatImages as $resultatImage) {
                             echo "<div class='realisation imagier'><img src='imagier/$resultatImage'/>
                             </div> ";
