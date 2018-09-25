f51131d0672536efb8e88a54eefdbf08b069fbe3
Eric Leroy-Terquem <ericleroyterquem@yahoo.fr>
2018-09-25 23:31:30 +0200

    update contact.php

--- contact.php
+++ contact.php
@@ -6,8 +6,25 @@ $realisations_active = '';
 $achat_active = '';
 $a_propos_active = '';
 $contact_active = 'active';
-require_once('inc/header.realisation.inc.php');	
+require_once('inc/header.realisation.inc.php');
 
+if(isset($_POST['Envoyer'])){
+    $to = "ericleroyterquem@yahoo.fr"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $first_name = $_POST['prenom'];
+    $last_name = $_POST['nom'];
+    $subject = "Form submission";
+    $subject2 = "Copy of your form submission";
+    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    }
 ?>
 
             <div id ="identificationContenu">
@@ -17,7 +34,7 @@ require_once('inc/header.realisation.inc.php');
                 <h4><a href="http://eepurl.com/mrZfT">Inscription à la newsletter</a></h4>
                 <h4><a href="https://www.facebook.com/pg/BertrandSegersGloshmol/" target="_blank">Facebook</a></h4>
                 <h4><a href="https://vimeo.com/user71683044" target="_blank">Vimeo</a></h4>
-                <form method="get" action="#" enctype="multipart/form-data">
+                <form method="post" action="#" enctype="multipart/form-data">
                     <label for="nom">Nom</label>
                     <input type="text" id="nom" name="nom" placeholder="Votre nom" required /><br><br>
 
