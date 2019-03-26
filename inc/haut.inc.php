06a2735c2c54acaf8f7866984529224e08a0e18c
Eric Leroy-Terquem <ericleroyterquem@yahoo.fr>
2019-03-26 23:40:05 +0100

    add a google analytics script

--- inc/haut.inc.php
+++ inc/haut.inc.php
@@ -15,6 +15,19 @@
     <link rel="stylesheet" href="css/jquery.lazyloadxt.spinner.css">
 <!--     <link rel="stylesheet" href="<?= RACINE_SITE ?>css/animate.css" /> 
  -->    <!-- <script src="js/jquery.fullpage.js"></script> -->
+    <script type="text/javascript">
+
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-36335015-1']);
+        _gaq.push(['_trackPageview']);
+
+        (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+
+    </script>
 </head>
 
 <body>
