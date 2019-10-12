f9bf6a2c42600c4df6b0944addbf4e03be9916d6
Eric Leroy-Terquem <ericleroyterquem@yahoo.fr>
2019-10-12 23:06:05 +0200

    complete script for 600 px images and reduce quality

--- imagier_process.php
+++ imagier_process.php
@@ -7,25 +7,48 @@ if ($_GET['run']) {
     $imagesBrutes = array_slice($imagesBrutes, 2);
 
     foreach ($imagesBrutes as $imageBrute) {
-        $im = new imagick("imagierbrut/$imageBrute");
-        $imageprops = $im->getImageGeometry();
+        $imHD = new imagick("imagierbrut/$imageBrute");
+        $imBD = new imagick("imagierbrut/$imageBrute");
+        $imageprops = $imHD->getImageGeometry();
         $width = $imageprops['width'];
         $height = $imageprops['height'];
         if($width > $height){
-            $newWidth = 900;
-            $newHeight = (900 / $width) * $height;
+            $newHDWidth = 900;
+            $newBDWidth = 600;
+            $newHDHeight = (900 / $width) * $height;
+            $newBDHeight = (600 / $width) * $height;
           }else{
-            $newHeight = 900;
-            $newWidth = (900 / $height) * $width;
+            $newHDHeight = 900;
+            $newBDHeight = 600;
+            $newHDWidth = (900 / $height) * $width;
+            $newBDWidth = (600 / $height) * $width;
         }
-        $im->resizeImage($newWidth,$newHeight, imagick::FILTER_LANCZOS, 0.9, true);
-        $im->writeImage("imagierhdtest/$imageBrute");
+        $imHD->resizeImage($newHDWidth,$newHDHeight, imagick::FILTER_LANCZOS, 0.9, true);
+        $imBD->resizeImage($newBDWidth,$newBDHeight, imagick::FILTER_LANCZOS, 0.9, true);
+        $imHD->writeImage("imagierhdtest/$imageBrute");
+        $imBD->writeImage("imagierbdtest/$imageBrute");
+        $imHDResource = imagecreatefromjpeg("imagierhdtest/$imageBrute");
+        $imBDResource = imagecreatefromjpeg("imagierbdtest/$imageBrute");
+        imagejpeg($imHDResource, "imagierhdtest/$imageBrute", 75);
+        imagejpeg($imBDResource, "imagierbdtest/$imageBrute", 75);
+
         echo "photo $imageBrute convertie en 900px";
         echo "<br><br>";
         echo '<img src="imagierhdtest/' . $imageBrute . '">';        
         echo "<br><br><br><br>";
+
+        echo "photo $imageBrute convertie en 600px";
+        echo "<br><br>";
+        echo '<img src="imagierbdtest/' . $imageBrute . '">';        
+        echo "<br><br><br><br>";
     }
 
+    // $files = glob('imagierbrut/*'); // get all file names
+    // foreach($files as $file){ // iterate files
+    //     if(is_file($file))
+    //         unlink($file); // delete file
+    // }
+    // echo "dossier imagierbrut vidé";
 }
 ?>
 
